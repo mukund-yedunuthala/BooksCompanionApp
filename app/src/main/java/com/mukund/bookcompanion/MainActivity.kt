f401f1ecd3c2fa73230a25ac776b03facd2d25c8
Mukund Yedunuthala <mukund.yedunuthala@outlook.de>
2023-03-10 16:36:25 +0100

    add animations to settings and libraries screens - add new dependency accompanist navigation animation

--- app/src/main/java/com/mukund/bookcompanion/MainActivity.kt
+++ app/src/main/java/com/mukund/bookcompanion/MainActivity.kt
@@ -3,20 +3,23 @@ package com.mukund.bookcompanion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.mukund.bookcompanion.navigation.NavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
-            NavGraph(navController = rememberNavController())
+            NavGraph(navController = rememberAnimatedNavController())
         }
     }
 }
--- app/src/main/java/com/mukund/bookcompanion/navigation/NavGraph.kt
+++ app/src/main/java/com/mukund/bookcompanion/navigation/NavGraph.kt
@@ -1,13 +1,16 @@
 package com.mukund.bookcompanion.navigation
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType.Companion.IntType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.AnimatedNavHost
 import com.mukund.bookcompanion.core.Constants.Companion.BOOK_ID
 import com.mukund.bookcompanion.navigation.Screen.BooksScreen
 import com.mukund.bookcompanion.navigation.Screen.UpdateBookScreen
@@ -19,14 +22,14 @@ import com.mukund.bookcompanion.ui.settings.SettingScreen
 import com.mukund.bookcompanion.ui.settings.components.LibsScreen
 import com.mukund.bookcompanion.ui.theme.BooksCompanionTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun NavGraph(
     navController: NavHostController
 ) {
     BooksCompanionTheme() {
         Surface {
-            NavHost(
+            AnimatedNavHost(
                 navController = navController,
                 startDestination = BooksScreen.route
             ) {
@@ -60,13 +63,48 @@ fun NavGraph(
                 }
                 composable(
                     route = SettingsScreen.route,
+                    enterTransition = {
+                        slideInHorizontally(
+                            initialOffsetX = {300},
+                            animationSpec = tween(300, easing = FastOutSlowInEasing)
+                        ) +
+                                fadeIn(animationSpec = tween(300))
+                    },
+                    exitTransition = {
+                        slideOutHorizontally(
+                            targetOffsetX = {-300},
+                            animationSpec = tween(300, easing = FastOutSlowInEasing)
+                        ) +
+                        fadeOut(animationSpec = tween(300))
+                    },
+                    popEnterTransition = {
+                        slideInHorizontally(
+                            initialOffsetX = {300},
+                            animationSpec = tween(300, easing = FastOutSlowInEasing)
+                        ) +
+                        fadeIn(animationSpec = tween(300))
+                    }
                 ) {
                     SettingScreen (
                         backPress = { navController.popBackStack() }
                     ) { navController.navigate(LibrariesScreen.route) }
                 }
                 composable(
-                    route = LibrariesScreen.route
+                    route = LibrariesScreen.route,
+                    exitTransition = {
+                        slideOutHorizontally(
+                            targetOffsetX = {-300},
+                            animationSpec = tween(300, easing = FastOutSlowInEasing)
+                        ) +
+                                fadeOut(animationSpec = tween(300))
+                    },
+                    enterTransition = {
+                        slideInHorizontally(
+                            initialOffsetX = {300},
+                            animationSpec = tween(300, easing = FastOutSlowInEasing)
+                        ) +
+                                fadeIn(animationSpec = tween(300))
+                    }
                 ) {
                     LibsScreen {
                         navController.popBackStack()
