070de17dc2040392697ffd732c4068a9c742a7c9
Mukund Yedunuthala <mukund.yedunuthala@outlook.de>
2023-08-20 18:10:21 +0200

    chore(Application): optimize imports

--- app/src/main/java/com/mukund/bookcompanion/ui/settings/components/CustomOSSLibsDialog.kt
+++ app/src/main/java/com/mukund/bookcompanion/ui/settings/components/CustomOSSLibsDialog.kt
@@ -1,16 +1,18 @@
 package com.mukund.bookcompanion.ui.settings.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 
