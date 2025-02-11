ba3553c077ff9ab8688900f10ea5fdc06de1a744
Mukund Yedunuthala <mukundkash1997@gmx.net>
2025-02-11 15:22:43 +0100

    chore: upgrade AGP to 8.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId="com.mukund.bookcompanion"
         minSdk=29
         targetSdk=34
-        versionCode=25
-        versionName="0.1.4"
+        versionCode=26
+        versionName="0.1.5"
 
         vectorDrawables {
             useSupportLibrary = true
